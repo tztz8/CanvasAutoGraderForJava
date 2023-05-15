f8e72be45f24b5ee145382e95b21397c59e755b6
Timbre Freeman <tfreeman3@ewu.edu>
2023-05-15 13:54:44 -0700

    clean up message

--- python/student_helper.py
+++ python/student_helper.py
@@ -27,14 +27,18 @@ class student_object:
         messages = ""
         for message in self.message:
             messages += message
-            messages += "\nMessage:"
-        messages += " No more messages"
-        message_string = "Auto Grader Output for {username}: \n" \
-                         "Message: {messages} \n" \
-                         "Results Collaborators({nbcpoints}%)(" \
-                         "Why: do you remove the class from collaborators on GitHub): {nbc}\n " \
-                         "JUnit Score({junitpoints}%)(Why: do you pass junit tests): {junit}\n " \
-                         "CheckStyle Score({checkpoints}%)(Why: do you use final, this, override): {check}"
+            messages += "\nMessage: "
+        messages += "No more messages"
+        message_string = "Auto Grader Output for {username}:\n\n" \
+                         "Message: {messages}\n\n" \
+                         "----------------------------------------------------------------------------------------\n" \
+                         "Results\n" \
+                         "Collaborators Score({nbcpoints}% weight) " \
+                         "(What score for: if you remove the class from collaborators on GitHub): {nbc}\n" \
+                         "JUnit Score({junitpoints}% weight) " \
+                         "(What score for: how much junit tests you pass): {junit}\n" \
+                         "CheckStyle Score({checkpoints}% weight) " \
+                         "(What score for: if you use final, this, override): {check}"
         out_of = grade_weight.Collaborators_Weight
         collaborators_result = (max(0, (out_of - self.num_of_bad_collaborators)) / out_of) * 100
         out_message = message_string.format(username=self.github_user_name,
