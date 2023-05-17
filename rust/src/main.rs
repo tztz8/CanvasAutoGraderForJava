a06cf21240c4bf9c2299b948bc88d0786d3d52b5
Timbre Freeman <tfreeman3@ewu.edu>
2023-05-16 18:05:46 -0700

    rust fixed

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,5 @@
-use std::task::Poll;
 use canvasapi::canvas::CanvasInformation;
-use canvasapi::models::course;
-use canvasapi::prelude::{Canvas, Course};
+use canvasapi::prelude::{Canvas};
 use config::Config;
 
 #[tokio::main]
@@ -19,11 +17,13 @@ async fn main() {
     //     .expect("No Canvas Token in Secret Config"));
 
 
-    let base_url = "https://canvas.ewu.edu/";
+    let base_url:String = secret.get("base_url")
+        .expect("no base url in Secret Config");
 
-    let canvas_token = "10~Wo5rqgrPaBV9v21Bz08jmR9cBLyUfIWUhrVDcBeJlrAspseq5b6mzBndfhdnd53C";
+    let canvas_token:String = secret.get("canvas_token")
+        .expect("no canvas_token in Secret Config");
 
-    let canvas = CanvasInformation::new(base_url, canvas_token);
+    let canvas = CanvasInformation::new(&base_url, &canvas_token);
 
     let course = Canvas::get_course(secret.get("course_id")
         .expect("No Course ID Given in Secret Config"))
@@ -34,9 +34,30 @@ async fn main() {
 
     let assignment = course.get_assignment(
         secret.get("assignment_id")
-            .expect("no assignment"))
-        .expect("No Assignment")
+            .expect("no assignment in Secret Config"))
+        .expect("No Assignment in course")
         .fetch(&canvas).await.expect("unable").inner();
+
+    let submissions = assignment.get_submissions()
+        .expect("No submissions")
+        .fetch(&canvas).await.expect("unable").inner();
+
+    let users = course.get_users()
+        .expect("get users")
+        .fetch(&canvas).await.expect("unable").inner();
+
+    for user in users {
+        let user_name = user.name.expect("no name");
+        let user_id = user.id;
+        let mut s_grade = String::from("no grade");
+        for submission in &submissions {
+            if submission.user_id.expect("no id") == user_id {
+                s_grade = submission.grade.clone().expect("no grade");
+                break;
+            }
+        }
+        println!("Found {user_name}, ID: {user_id}, Grade: {s_grade}");
+    }
 
     let a_name = assignment.name.expect("no name");
     let a_due = assignment.due_at.expect("no due");
